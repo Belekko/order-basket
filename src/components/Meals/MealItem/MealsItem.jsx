dc1aa94509bec737a260e3199e824880537f338f
Belekko <97427600+Belekko@users.noreply.github.com>
2022-05-12 12:22:26 +0600

    feature/MealsItemWithStyle

---
+++ src/components/Meals/MealItem/MealsItem.jsx
@@ -0,0 +1,17 @@
+import classes from './MealItem.module.css';
+import React from 'react';
+
+const MealsItem = (props) => {
+  const price= `$ ${props.price.toFixed(2)}`
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.discription}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+    </li>
+  );
+};
+
+export default MealsItem;
